dcc15f293887989a7fcb8a1df0768bab80a1a075
Doug Stensrud <dwsappend@gmail.com>
2024-10-15 12:54:28 -0700

    Push the value www.awesomelead.com to myArray when the input button is clicked

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// Create two variables:
-// myLeads -> should be assigned to an empty array
-// inputEl -> should be assigned to the text input field
-
-let inputBtn = document.getElementById("input-btn");
 let myLeads = [];
-let inputEl = document.getElementById("input-el");
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+
+// Push the value "www.awesomelead.com" to myArray when the input button is clicked
 
 inputBtn.addEventListener("click", function () {
+  myLeads.push("www.awesomelead.com");
   console.log("Button clicked!");
+  console.log(myLeads);
 });
