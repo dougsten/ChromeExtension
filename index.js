bf10be3b08011c50778f70ec749bed36617ac8e4
Doug Stensrud <dwsappend@gmail.com>
2024-10-15 14:13:45 -0700

    Grab the unordered list and store it in a const variable called ulEl

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+// 2. Grab the unordered list and store it in a const variable called ulEl
+ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   console.log(myLeads);
 });
 
-// Log out the items in the myLeads array using a for loop
 for (let i = 0; i < myLeads.length; i++) {
   console.log(myLeads[i]);
 }
