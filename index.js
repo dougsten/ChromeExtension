f7906884ab7ecda4e5cf064ba4b5157d37205453
Doug Stensrud <dwsappend@gmail.com>
2024-10-17 10:24:59 -0700

    Add clear button for personal preference (not part of course)

--- index.js
+++ index.js
@@ -2,21 +2,32 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const clearBtn = document.getElementById("clear-btn");
 
-// Get the leads from the localStorage - PS: JSON.parse()
-// Store it in a variable, leadsFromLocalStorage
-// Log out the variable
-  let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
-  console.log(leadsFromLocalStorage);
+// ["lead1", "lead2"] or null
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+  if (leadsFromLocalStorage){
+    console.log(leadsFromLocalStorage);
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+  }
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
+});
 
-  // To verify that it works:
-  console.log(localStorage.getItem("myLeads"));
+clearBtn.addEventListener("click", function () {
+  myLeads = [];
+  localStorage.clear();
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
 });
 
 function renderLeads() {
