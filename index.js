bf68b933bfec903b492188d108f60a7130f1bea6
Doug Stensrud <dwsappend@gmail.com>
2024-10-17 07:16:44 -0700

    Turn the myLeads string into an array

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-let myLeads = [];
+let myLeads = `["www.awesomelead.com"]`;
+
+// 1. Turn the myLeads string into an array
+  myLeads = JSON.parse(myLeads);
+// 2. Push a new value to the array
+  myLeads.push("www.awesomelead2.com");
+// 3. Turn the array into a string again
+  myLeads =   JSON.stringify(myLeads);
+// 4. Console.log the string using typeof to verify that it's a string
+  //console.log( typeof(myLeads) );
+  console.log( myLeads );
+
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-
-// 1. Save a key-value pair in localStorage
-  localStorage.setItem("name", "Doug");
-// 2. Refresh the page. Get the value and log it to the console
-  console.log(localStorage.getItem("name"));
-// 3. Clear localStorage
-  localStorage.clear();
-
-// HINTS:
-// localStorage.setItem(key, value)
-// localStorage.getItem(key)
-// localStorage.clear()
-// PS: both key and value need to be strings
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
